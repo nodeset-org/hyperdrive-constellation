c97e7df1fccbb3f56434267aebda5938997afcef
huy <huy@nodeset.io>
2024-07-11 11:04:41 -0700

    Add another test

--- internal/tests/api/with-ns-registered/minipool_test.go
+++ internal/tests/api/with-ns-registered/minipool_test.go
@@ -7,7 +7,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMinipoolGetCount_Zero(t *testing.T) {
+const (
+	expectedMinipoolCount int = 1
+)
+
+// Test getting the available minipool count when there are no minipools available
+func TestMinipoolGetAvailableMinipoolCount_Zero(t *testing.T) {
 	// Take a snapshot, revert at the end
 	snapshotName, err := testMgr.CreateCustomSnapshot(hdtesting.Service_EthClients | hdtesting.Service_Filesystem | hdtesting.Service_NodeSet)
 	if err != nil {
@@ -20,4 +25,24 @@ func TestMinipoolGetCount_Zero(t *testing.T) {
 	countResponse, err := cs.Minipool.GetAvailableMinipoolCount()
 	require.NoError(t, err)
 	require.Equal(t, 0, countResponse.Data.Count)
+}
+
+// Test getting the available minipool count when there is one minipool available
+func TestMinipoolGetAvailableMinipoolCount_One(t *testing.T) {
+	// Take a snapshot, revert at the end
+	snapshotName, err := testMgr.CreateCustomSnapshot(hdtesting.Service_EthClients | hdtesting.Service_Filesystem | hdtesting.Service_NodeSet)
+	if err != nil {
+		fail("Error creating custom snapshot: %v", err)
+	}
+	defer nodeset_cleanup(snapshotName)
+
+	// Set up the NodeSet mock server
+	nsMgr := testMgr.GetNodeSetMockServer().GetManager()
+	nsMgr.SetAvailableConstellationMinipoolCount(nodeAddress, expectedMinipoolCount)
+
+	// Check if the node is registered
+	cs := testMgr.GetApiClient()
+	countResponse, err := cs.Minipool.GetAvailableMinipoolCount()
+	require.NoError(t, err)
+	require.Equal(t, expectedMinipoolCount, countResponse.Data.Count)
 }
