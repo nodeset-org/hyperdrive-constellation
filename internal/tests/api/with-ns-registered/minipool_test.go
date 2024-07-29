7ecad82b359ce7d5b9405ab879abefe5550df760
huy <huy@nodeset.io>
2024-07-29 12:57:25 -0700

    wip

--- internal/tests/api/with-ns-registered/minipool_test.go
+++ internal/tests/api/with-ns-registered/minipool_test.go
@@ -60,6 +60,20 @@ func TestMinipoolDepositAndStake(t *testing.T) {
 	defer nodeset_cleanup(snapshotName)
 
 	depositAndStakeMinipool(t)
+	simulateEthRewardToYieldDistributor(t)
+}
+
+// Simulate an ETH reward getting deposited to YieldDistributor
+func simulateEthRewardToYieldDistributor(t *testing.T) {
+	// Send ETH to deposit pool
+	txMgr := testMgr.GetTransactionManager()
+	sendEthTx := txMgr.CreateTransactionInfoRaw(depositPoolAddress, nil, sendEthOpts)
+	MineTx(t, sendEthTx, deployerOpts, "Sent ETH to deposit pool")
+
+	slotsToAdvance := 1200 * 60 * 60 / 12
+	err := testMgr.AdvanceSlots(uint(slotsToAdvance), false)
+	require.NoError(t, err)
+	t.Logf("Advanced %d slots", slotsToAdvance)
 }
 
 // Makes a minipool and stakes it
